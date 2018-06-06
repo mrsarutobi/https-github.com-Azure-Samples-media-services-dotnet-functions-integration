0cad21378ea57163cb7f679867f4026c0f5c7e43
Shige <shigeyf@microsoft.com>
2018-06-07 03:00:23 +0900

    Text Encoding bug fix.

--- advanced-vod-workflow-functions/SubmitMediaJob.cs
+++ advanced-vod-workflow-functions/SubmitMediaJob.cs
@@ -124,7 +124,7 @@ namespace advanced_vod_functions
                     if (mediaTask.configuration.StartsWith(base64encodedstringprefix))
                     {
                         byte[] b64decoded = Convert.FromBase64String(mediaTask.configuration.Substring(base64encodedstringprefix.Length));
-                        mediaTask.configuration = System.Text.Encoding.Unicode.GetString(b64decoded);
+                        mediaTask.configuration = System.Text.ASCIIEncoding.ASCII.GetString(b64decoded);
                     }
                     task = job.Tasks.AddNew(mediaTask.mediaTaskName, processor, mediaTask.configuration, TaskOptions.None);
                     if (mediaTask.additionalInputAssetIds != null)
